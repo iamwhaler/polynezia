84816e5c2ae046632c7284395a69a2ec07c832eb
Alex Gapak <iamgapak@gmail.com>
2018-04-11 07:11:28 +0300

    Mine balancing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const resources = {
   'fish': {name: 'Fish', is_nature: true, locked_till: 'pier', difficulty: 1.5, max_cap: 10000, regen: 3},
   'wildfowl': {name: 'Meat', is_nature: true, locked_till: 'lodge', difficulty: 2, max_cap: 10000, regen: 4},
   'wood': {name: 'Wood', is_nature: true, locked_till: true, difficulty: 1, max_cap: 10000, regen: 2},
-  'stone': {name: 'Stone', is_nature: false, locked_till: 'quarry', difficulty: 20, max_cap: 2000, regen: 0.1},
+  'stone': {name: 'Stone', is_nature: false, locked_till: 'quarry', difficulty: 10, max_cap: 2500, regen: 0.1},
   'iron': {name: 'Iron', is_nature: false, locked_till: 'mine', difficulty: 100, max_cap: 500, regen: 0.01},
   'moai': {name: 'Moai', is_nature: false, locked_till: 'ahu', difficulty: 1000, max_cap: 42, regen: 0.0}
 };
@@ -20,14 +20,14 @@ const buildings = {
   'bonfire': {name: 'Bonfire', cost: {'wood': 10}, locked_till: 'hut', text: 'Attracts new residents.'},
   'hut': {name: 'Hut', cost: {'wood': 50}, locked_till: true, text: 'Home for Two.'},
   'house': {name: 'House', cost: {'wood': 100, 'stone': 10}, locked_till: 'mine', text: 'Home for Five.'},
-  'garden': {name: 'Garden', cost: {'fruits': 10}, locked_till: 'hut', text: 'Each garden accelerates the speed of one gardener.'},
-  'keep': {name: 'Keep', cost: {'wood': 100}, locked_till: 'garden', text: 'Each nature keep increases the regeneration of the resources of living nature.'},
-  'field': {name: 'Field', cost: {'wood': 100}, locked_till: 'keep', text: 'Each field accelerates the speed of one fielder.'},
-  'pier': {name: 'Pier', cost: {'wood': 100, 'stone': 10}, locked_till: 'quarry', text: 'Each pier accelerates the speed of one fisherman.'},
-  'lodge': {name: 'Lodge', cost: {'wood': 100, 'iron': 10}, locked_till: 'mine', text: 'Each lodge accelerates the speed of one hunter.'},
+  'garden': {name: 'Garden', cost: {'fruits': 10}, locked_till: 'hut', text: 'Provide fruits. Each garden accelerates the speed of one gardener.'},
+  'keep': {name: 'Keep', cost: {'wood': 100}, locked_till: 'garden', text: 'Increases living nature regeneration. Each field accelerates the speed of one keeper.'},
+  'field': {name: 'Field', cost: {'wood': 100}, locked_till: 'keep', text: 'Provide roots. Each field accelerates the speed of one fielder.'},
+  'pier': {name: 'Pier', cost: {'wood': 100, 'stone': 10}, locked_till: 'quarry', text: 'Provide fist. Each pier accelerates the speed of one fisherman.'},
+  'lodge': {name: 'Lodge', cost: {'wood': 100, 'iron': 10}, locked_till: 'mine', text: 'Provide hunt. Each lodge accelerates the speed of one hunter.'},
   'sawmill': {name: 'Sawmill', cost: {'wood': 250, 'iron': 10}, locked_till: 'mine', text: 'Each sawmill accelerates the speed of one woodcutter.'},
-  'quarry': {name: 'Quarry', cost: {'wood': 1000}, locked_till: 'bonfire', text: 'Each quarry accelerates the speed of one mason.'},
-  'mine': {name: 'Mine', cost: {'wood': 1000, 'stone': 100}, locked_till: 'quarry', text: 'Each mine accelerates the speed of one miner.'},
+  'quarry': {name: 'Quarry', cost: {'wood': 1000}, locked_till: 'bonfire', text: 'Provide stone. Each quarry accelerates the speed of one mason.'},
+  'mine': {name: 'Mine', cost: {'wood': 1000, 'stone': 100}, locked_till: 'quarry', text: 'Provide iron and stone. Each mine accelerates the speed of one miner.'},
   'ahu': {name: 'Ahu', cost: {'stone': 1000}, locked_till: 'mine', text: 'Each Ahu accelerates the speed of one builder.'},
 };
 
@@ -196,7 +196,11 @@ class App extends Component {
     _.each(professions, (profession, profession_key) => {
       if (profession.resource) {
         if (profession_key === 'miner') {
-          state['stone'] += this.state.mine;
+          for (let i=0; i<Math.min(this.state.mine, this.state.miner); i++) {
+            if (Math.floor(_.random(1, 10)) === 1) {
+              state['stone']++;
+            }
+          }
         }
         if (this.state[profession_key] > 0 && this.state[profession.resource+'_volume'] > 0) {
           let productivity = this.state[profession_key] + Math.min(this.state[profession_key], this.state[profession.home]);
